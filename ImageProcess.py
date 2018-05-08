0512778de776b3f42d4ac1a1847610a716b78f70
calton007 <18347082+calton007@users.noreply.github.com>
2018-05-08 13:39:36 +0800

    add photos

--- ImageProcess.py
+++ ImageProcess.py
@@ -58,24 +58,25 @@ class Graphics:
             shutil.copy(self.infile, self.outfile)  
 
   
-    def cut_by_ratio(self):  
-        """按照图片长宽进行分割
-        
-        ------------
-        取中间的部分，裁剪成正方形
-        """  
-        im = Image.open(self.infile)  
-        (x, y) = im.size  
-        if x > y:  
-            region = (int(x/2-y/2), 0, int(x/2+y/2), y)  
-            #裁切图片  
-            crop_img = im.crop(region)  
-            #保存裁切后的图片  
-            crop_img.save(self.outfile)             
-        elif x < y:  
-            region = (0, int(y/2-x/2), x, int(y/2+x/2))
-            #裁切图片  
-            crop_img = im.crop(region)  
-            #保存裁切后的图片  
-            crop_img.save(self.outfile)             
+    def cut_by_ratio(self): 
+        pass
 
+        # """按照图片长宽进行分割
+        
+        # ------------
+        # 取中间的部分，裁剪成正方形
+        # """  		
+        # im = Image.open(self.infile)  
+        # (x, y) = im.size  
+        # if x > y:  
+            # region = (int(x/2-y/2), 0, int(x/2+y/2), y)  
+            # #裁切图片  
+            # crop_img = im.crop(region)  
+            # #保存裁切后的图片  
+            # crop_img.save(self.outfile)             
+        # elif x < y:  
+            # region = (0, int(y/2-x/2), x, int(y/2+x/2))
+            # #裁切图片  
+            # crop_img = im.crop(region)  
+            # #保存裁切后的图片  
+            # crop_img.save(self.outfile)
