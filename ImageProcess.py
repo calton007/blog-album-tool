e997052d8499562ad09103ab7b8ba598f1aa85a1
Calton Li <caltonli@jinglingedu.com>
2026-05-17 20:36:40 +0800

    fix: make image tool run on current Python

--- ImageProcess.py
+++ ImageProcess.py
@@ -1,7 +1,7 @@
-# coding=utf-8  
-from PIL import Image  
-import shutil  
-import os  
+# coding=utf-8
+from PIL import Image
+import shutil
+import os
 
 class Graphics:  
     '''图片处理类
@@ -17,45 +17,45 @@ class Graphics:
 
     def fixed_size(self, width, height):  
         """按照固定尺寸处理图片"""  
-        im = Image.open(self.infile)  
-        out = im.resize((width, height),Image.ANTIALIAS)  
-        out.save(self.outfile)  
+        im = Image.open(self.infile)
+        out = im.resize((width, height), Image.Resampling.LANCZOS)
+        out.save(self.outfile)
 
 
     def resize_by_width(self, w_divide_h):  
         """按照宽度进行所需比例缩放"""  
-        im = Image.open(self.infile)  
-        (x, y) = im.size   
-        x_s = x  
-        y_s = x/w_divide_h  
-        out = im.resize((x_s, y_s), Image.ANTIALIAS)   
-        out.save(self.outfile)  
+        im = Image.open(self.infile)
+        (x, y) = im.size
+        x_s = x
+        y_s = int(x / w_divide_h)
+        out = im.resize((x_s, y_s), Image.Resampling.LANCZOS)
+        out.save(self.outfile)
 
 
     def resize_by_height(self, w_divide_h):  
         """按照高度进行所需比例缩放"""  
-        im = Image.open(self.infile)  
-        (x, y) = im.size   
-        x_s = y*w_divide_h  
-        y_s = y  
-        out = im.resize((x_s, y_s), Image.ANTIALIAS)   
-        out.save(self.outfile)  
+        im = Image.open(self.infile)
+        (x, y) = im.size
+        x_s = int(y * w_divide_h)
+        y_s = y
+        out = im.resize((x_s, y_s), Image.Resampling.LANCZOS)
+        out.save(self.outfile)
 
 
     def resize_by_size(self, size):  
         """按照生成图片文件大小进行处理(单位KB)"""  
         size *= 1024  
-        im = Image.open(self.infile)  
-        size_tmp = os.path.getsize(self.infile)  
-        q = 100  
-        while size_tmp > size and q > 0:  
-            print (q)  
-            out = im.resize(im.size, Image.ANTIALIAS)  
-            out.save(self.outfile, quality=q)  
-            size_tmp = os.path.getsize(self.outfile)  
-            q -= 5  
-        if q == 100:  
-            shutil.copy(self.infile, self.outfile)  
+        im = Image.open(self.infile)
+        size_tmp = os.path.getsize(self.infile)
+        q = 100
+        while size_tmp > size and q > 0:
+            print(q)
+            out = im.resize(im.size, Image.Resampling.LANCZOS)
+            out.save(self.outfile, quality=q)
+            size_tmp = os.path.getsize(self.outfile)
+            q -= 5
+        if q == 100:
+            shutil.copy(self.infile, self.outfile)
 
   
     def cut_by_ratio(self): 
